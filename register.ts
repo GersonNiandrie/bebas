12d8d7a5350d5db2d961b04d8bb8e403e879f024
masdefry <muhammaddefryan@gmail.com>
2025-11-28 10:50:35 +0700

    fix(register): change variable

--- register.ts
+++ register.ts
@@ -1,6 +1,9 @@
 const USERNAME = 'Rafathar';
-console.log(USERNAME || !USERNAME)
-
-console.log(
-  `akun terbuat dengan USERNAME: ${USERNAME}`
-);
+console.log(USERNAME || !USERNAME);
+const PASSWORD = 'abc12345';
+const HASHEDPASSWORD = 'asaksakskjaska';
+console.log(`akun terbuat dengan USERNAME: ${USERNAME}`);
+console.log(`akun terbuat dengan USERNAME: ${USERNAME}`);
+console.log(`akun terbuat dengan USERNAME: ${USERNAME}`);
+console.log(`akun terbuat dengan USERNAME: ${USERNAME}`);
+console.log(`akun terbuat dengan USERNAME: ${USERNAME}`);
