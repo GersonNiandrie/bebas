5c71ff59e1beb1fc8f9574b942bd01d3966bdd07
Gerson Niandrie <Gniandrie@gmail.com>
2025-11-28 10:38:46 +0700

    console log message update

--- login.ts
+++ login.ts
@@ -1,5 +1,5 @@
 const username = 'Gerson';
 const password = 'kdxcz23';
 
-console.log(username);
-console.log(password);
+console.log(`Masukan username : ${username}`);
+console.log(`Masukan password : ${password}`);
